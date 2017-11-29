a56c8279241fe4cf5d83bdb961cd735f1e8e7f41
vjrj <vjrj@ourproject.org>
2017-11-29 15:57:48 +0100

    Playing with materize

--- client/main.js
+++ client/main.js
@@ -1,4 +1,3 @@
 import '../imports/startup/client';
-/*
-import '../node_modules/daemonite-material/js/material.min.js';
-import '../node_modules/daemonite-material/css/material.css';*/
+/* import '../node_modules/daemonite-material/js/material.min.js';
+import '../node_modules/daemonite-material/css/material.css'; */
