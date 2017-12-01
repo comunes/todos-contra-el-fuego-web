f87cf02c2d1f7e5ddb79080247d9ba54b8b0d7df
vjrj <vjrj@ourproject.org>
2017-12-01 12:57:59 +0100

    Remove log

--- imports/startup/client/piwik-start.js
+++ imports/startup/client/piwik-start.js
@@ -2,7 +2,7 @@ Meteor.startup(function() {
   return Tracker.autorun(function() {
     var userId;
     userId = Meteor.userId();
-    console.log(userId);
+    // console.log(userId);
     Meteor.Piwik.setUserInfo(userId);
   });
 });
