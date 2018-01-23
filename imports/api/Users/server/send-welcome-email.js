96c524a90c75422da508f605bb6d5a1a2b99c653
vjrj <vjrj@ourproject.org>
2018-01-23 20:29:36 +0100

    Only send emails to users with emails

--- imports/api/Users/server/send-welcome-email.js
+++ imports/api/Users/server/send-welcome-email.js
@@ -1,3 +1,4 @@
+import { Meteor } from 'meteor/meteor';
 import i18n from 'i18next';
 import sendEmail from '../../../modules/server/send-email';
 import getOAuthProfile from '../../../modules/get-oauth-profile';
@@ -9,20 +10,20 @@ export default (options, user, lang) => {
   const applicationName = i18n.t('AppName');
   const firstName = OAuthProfile ? OAuthProfile.name.first : options.profile.name.first;
   const emailAddress = OAuthProfile ? OAuthProfile.email : options.email;
-
-  return sendEmail({
-    to: emailAddress,
-    from: `${applicationName} <noreply@comunes.org>`,
-    subject: `[${applicationName}] Welcome, ${firstName}!`,
-    lang,
-    template: 'welcome',
-    templateVars: {
-      applicationName,
-      firstName,
-      welcomeUrl: Meteor.absoluteUrl('subscriptions') // e.g., returns http://localhost:3000/documents
-    }
-  })
-    .catch((error) => {
+  if (emailAddress) {
+    sendEmail({
+      to: emailAddress,
+      from: `${applicationName} <noreply@comunes.org>`,
+      subject: `[${applicationName}] Welcome, ${firstName}!`,
+      lang,
+      template: 'welcome',
+      templateVars: {
+        applicationName,
+        firstName,
+        welcomeUrl: Meteor.absoluteUrl('subscriptions') // e.g., returns http://localhost:3000/documents
+      }
+    }).catch((error) => {
       throw new Meteor.Error('500', `${error}`);
     });
+  }
 };
