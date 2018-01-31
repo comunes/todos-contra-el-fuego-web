68aba3b9c145301baa2e4ab519754d8767d02bc9
vjrj <vjrj@ourproject.org>
2018-01-31 10:56:50 +0100

    Added facts documentation

--- imports/ui/pages/Status/Status.js
+++ imports/ui/pages/Status/Status.js
@@ -8,6 +8,13 @@ import { translate } from 'react-i18next';
 import { withTracker } from 'meteor/react-meteor-data';
 import Blaze from 'meteor/gadicc:blaze-react-component';
 
+/*
+   https://github.com/meteor/docs/blob/version-NEXT/long-form/oplog-observe-driver.md
+   (...)
+   Now look at your app. The facts template will render a variety of metrics; the ones we're looking for are observe-drivers-oplog and observe-drivers-polling in the mongo-livedata section. If observe-drivers-polling is zero or not rendered at all, then all of your observeChanges calls are using OplogObserveDriver!
+   (...)
+*/
+
 class Status extends Component {
   constructor(props) {
     super(props);
