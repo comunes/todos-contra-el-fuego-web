458627f520fe64b1a2c09ddb7afa05fd321ad14f
vjrj <vjrj@ourproject.org>
2018-02-18 18:43:40 +0100

    Fix reconnect translation issues

--- imports/ui/components/Reconnect/Reconnect.js
+++ imports/ui/components/Reconnect/Reconnect.js
@@ -1,12 +1,33 @@
-import React from 'react';
+/* eslint-disable react/jsx-indent-props */
+/* eslint-disable import/no-absolute-path */
+
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { translate } from 'react-i18next';
 import { Meteor } from 'meteor/meteor';
 import { Tracker } from 'meteor/tracker';
 import Blaze from 'meteor/gadicc:blaze-react-component';
 
-const Reconnect = props => (
-  <Blaze template="meteorStatus" lang={props.i18n.language} />
-);
+class Reconnect extends Component {
+  render() {
+    const { t } = this.props;
+    return (
+      <Blaze
+          template="meteorStatus"
+          textDisconnect={t('Desconectado del servidor, reconectando en %delay% segundos.')}
+          textConnecting={t('Desconectado del servidor, reconectando...')}
+          linkText={t('Reintentar ahora')}
+      />
+    );
+  }
+}
+
+Reconnect.propTypes = {
+  t: PropTypes.func.isRequired
+};
+
+Reconnect.defaultProps = {
+};
 
 export default translate([], { wait: true })(Reconnect);
 
