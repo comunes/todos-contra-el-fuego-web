8b0577b46f4bae1c1b60b1ea80e56525e54c33f2
vjrj <vjrj@ourproject.org>
2018-03-03 10:54:53 +0100

    Refactor Reconnect

--- imports/ui/components/Reconnect/Reconnect.js
+++ imports/ui/components/Reconnect/Reconnect.js
@@ -1,30 +1,24 @@
 /* eslint-disable react/jsx-indent-props */
 /* eslint-disable import/no-absolute-path */
 
-import React, { Fragment, Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { translate } from 'react-i18next';
 import { Meteor } from 'meteor/meteor';
 import { Tracker } from 'meteor/tracker';
 import Blaze from 'meteor/gadicc:blaze-react-component';
 
-class Reconnect extends Component {
-  render() {
-    const { t, authenticated } = this.props;
-    // console.log(`Reconnect ${!authenticated ? 'enabled' : 'disabled'}`);
-    return (
-      <Fragment>
-        { !authenticated &&
-        <Blaze
+const Reconnect = ({ t, authenticated }) => (
+  <div>
+    { !authenticated &&
+      <Blaze
           template="meteorStatus"
           textDisconnect={t('Desconectado del servidor, reconectando en %delay% segundos.')}
           textConnecting={t('Desconectado del servidor, reconectando...')}
           linkText={t('Reintentar ahora')}
-        /> }
-      </Fragment>
-    );
-  }
-}
+      /> }
+  </div>
+);
 
 Reconnect.propTypes = {
   t: PropTypes.func.isRequired,
@@ -36,11 +30,12 @@ Reconnect.defaultProps = {
 
 export default translate([], { wait: true })(Reconnect);
 
+/*
 if (!Meteor.isProduction) {
   // We clear the console on disconnect during development
   Tracker.autorun(() => {
     if (Meteor.status().status === 'waiting') {
-      // console.clear();
+      console.clear();
     }
   });
-}
+} */
