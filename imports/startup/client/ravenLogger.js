db964d247ffdd4ffdeb2e679b4db29032eae8ada
vjrj <vjrj@ourproject.org>
2018-03-04 12:41:46 +0100

    Disabled raven in dev

--- imports/startup/client/ravenLogger.js
+++ imports/startup/client/ravenLogger.js
@@ -3,10 +3,12 @@ import { Meteor } from 'meteor/meteor';
 
 const ravenOptions = {};
 
-const ravenLogger = new RavenLogger({
+const enabled = true && !Meteor.isDevelopment;
+
+const ravenLogger = enabled ? new RavenLogger({
   publicDSN: Meteor.settings.public.sentryPublicDSN, // will be used on the client
   shouldCatchConsoleError: true, // default
   trackUser: true // default
-}, ravenOptions);
+}, ravenOptions) : { log: (error) => { console.log(error); } };
 
 export default ravenLogger;
