b92e3c9b302942843e888e04afc41fc1dd81d979
vjrj <vjrj@ourproject.org>
2018-03-13 01:46:46 +0100

    Added from now title

--- imports/ui/components/FromNow/FromNow.js
+++ imports/ui/components/FromNow/FromNow.js
@@ -5,6 +5,10 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { withTracker } from 'meteor/react-meteor-data';
 import Chronos from '/imports/ui/components/Chronos/Chronos';
+import { translate } from 'react-i18next';
+import { dateLongFormat } from '/imports/api/Common/dates';
+
+import './FromNow.scss';
 
 class FromNow extends Component {
   constructor(props) {
@@ -27,26 +31,39 @@ class FromNow extends Component {
     return !(nextState.when === this.state.when);
   }
 
+  thatIs() {
+    return this.props.t('esDecirTalDia', { date: dateLongFormat(this.props.whenDate) });
+  }
+
   render() {
     console.log(`Render from now ${this.state.when}`);
     return (
-      <span>{this.state.when}</span>
+      <span data-toggle="tooltip" className="from-now" title={this.thatIs()}>{this.state.when}</span>
     );
   }
 }
 
 FromNow.propTypes = {
-  when: PropTypes.string
+  t: PropTypes.func.isRequired,
+  when: PropTypes.string,
+  whenDate: PropTypes.instanceOf(Date)
 };
 
 FromNow.defaultProps = {
 };
 
-const FromNowContainer = withTracker((props) => {
+/*
+ * const FromNowContainer = withTracker((props) => {
+ *
+ * })(FromNow);
+ *
+ * export default FromNowContainer;
+ * */
+
+export default translate([], { wait: true })(withTracker((props) => {
   const whenDate = props.when;
   return {
-    when: whenDate ? Chronos.moment(whenDate).fromNow() : null
+    when: whenDate ? Chronos.moment(whenDate).fromNow() : null,
+    whenDate
   };
-})(FromNow);
-
-export default FromNowContainer;
+})(FromNow));
