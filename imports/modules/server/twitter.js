37928238dfa0959f9a67c987671431568c9d45cd
vjrj <vjrj@ourproject.org>
2018-03-17 01:42:48 +0100

    Added twitter tweet support

---
+++ imports/modules/server/twitter.js
@@ -0,0 +1,26 @@
+import { Meteor } from 'meteor/meteor';
+import Twit from 'twit';
+
+const confTwitter = (lang) => {
+  const tConf = Meteor.settings.private.twitter[lang];
+
+  const T = new Twit({
+    consumer_key: tConf.consumer_key,
+    consumer_secret: tConf.consumer_secret,
+    access_token: tConf.access_token,
+    access_token_secret: tConf.access_token_secret
+    // timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+  });
+
+  return T;
+};
+
+const tweet = (message, lang) => {
+  const T = confTwitter(lang);
+  T.post('statuses/update', { status: message }, (err, data, response) => {
+    // console.log(data);
+    // console.log(response);
+  });
+};
+
+export default tweet;
