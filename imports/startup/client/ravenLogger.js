d30385b49e181051d03c4130e360bc0f37606068
vjrj <vjrj@ourproject.org>
2018-05-21 17:42:37 +0200

    Disable raven when not configured

--- imports/startup/client/ravenLogger.js
+++ imports/startup/client/ravenLogger.js
@@ -2,11 +2,11 @@ import RavenLogger from 'meteor/flowkey:raven';
 import { Meteor } from 'meteor/meteor';
 
 const ravenOptions = {};
-
-const enabled = true && !Meteor.isDevelopment;
+const publicDSN = Meteor.settings.public.sentryPublicDSN;
+const enabled = !Meteor.isDevelopment && publicDSN;
 
 const ravenLogger = enabled ? new RavenLogger({
-  publicDSN: Meteor.settings.public.sentryPublicDSN, // will be used on the client
+  publicDSN, // will be used on the client
   shouldCatchConsoleError: true, // default
   trackUser: true // default
 }, ravenOptions) : { log: (error) => { console.log(error); } };
--- imports/startup/server/ravenLogger.js
+++ imports/startup/server/ravenLogger.js
@@ -2,12 +2,14 @@ import RavenLogger from 'meteor/flowkey:raven';
 import { Meteor } from 'meteor/meteor';
 
 const ravenOptions = {};
+const publicDSN = Meteor.settings.public.sentryPublicDSN;
+const privateDSN = Meteor.settings.sentryPrivateDSN;
 
-const ravenLogger = new RavenLogger({
-  publicDSN: Meteor.settings.public.sentryPublicDSN,
-  privateDSN: Meteor.settings.sentryPrivateDSN,
+const ravenLogger = publicDSN && privateDSN ? new RavenLogger({
+  publicDSN,
+  privateDSN,
   shouldCatchConsoleError: true, // default true
   trackUser: true // default false
-}, ravenOptions);
+}, ravenOptions) : { log: (error) => { console.log(error); } };
 
 export default ravenLogger;
