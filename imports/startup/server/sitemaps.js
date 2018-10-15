8b598fab7cb6b6217963c0f42ee9db685817d860
vjrj <vjrj@ourproject.org>
2018-10-15 06:02:05 +0200

    Disabled fires render

--- imports/startup/server/sitemaps.js
+++ imports/startup/server/sitemaps.js
@@ -4,6 +4,8 @@
 import Fires from '/imports/api/Fires/Fires';
 // import Comments from '/imports/api/Comments/Comments';
 
+const firesMapEnabled = false;
+
 sitemaps.add('/sitemap.xml', () => {
   const today = new Date();
 
@@ -20,21 +22,23 @@ sitemaps.add('/sitemap.xml', () => {
 
   // When user has public page
   /*  const users = Meteor.users.find().fetch();
-  _.each(users, function(user) {
-    out.push({
-      page: '/persona/' + user.username,
-      lastmod: user.updatedAt
-    });
+     _.each(users, function(user) {
+     out.push({
+     page: '/persona/' + user.username,
+     lastmod: user.updatedAt
+     });
      }); */
 
-  Fires.find({}, { limit: 100, sort: { createdAt: -1 } }).fetch().forEach((fire) => {
-    // Search the last comment of tha fire
-    const lastComment = Comments.getCollection().findOne({ referenceId: `fire-${fire._id}` }, { sort: { createdAt: -1 } });
-    out.push({
-      page: `/fire/archive/${fire._id._str}`,
-      lastmod: lastComment ? lastComment.createdAt : fire.updatedAt
+  if (firesMapEnabled) {
+    Fires.find({}, { limit: 100, sort: { createdAt: -1 } }).fetch().forEach((fire) => {
+      // Search the last comment of tha fire
+      const lastComment = Comments.getCollection().findOne({ referenceId: `fire-${fire._id}` }, { sort: { createdAt: -1 } });
+      out.push({
+        page: `/fire/archive/${fire._id._str}`,
+        lastmod: lastComment ? lastComment.createdAt : fire.updatedAt
+      });
     });
-  });
+  }
 
   return out;
 });
