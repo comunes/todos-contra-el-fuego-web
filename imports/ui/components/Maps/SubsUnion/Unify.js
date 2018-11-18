5f9a84dfd7627881cc75a216896c46d6cd41b1de
vjrj <vjrj@ourproject.org>
2018-11-18 15:45:14 +0100

    Unify don't calc boundaries of empty union

--- imports/ui/components/Maps/SubsUnion/Unify.js
+++ imports/ui/components/Maps/SubsUnion/Unify.js
@@ -2,7 +2,8 @@ import { calcUnion as calcUnionWoBounds } from 'map-common-utils';
 
 const calcUnion = (L, subs, decorated, typeCircle) => {
   const unionJson = calcUnionWoBounds(subs, decorated, typeCircle);
-  return [unionJson, L.geoJSON(unionJson).getBounds()];
+  // return [unionJson, L.geoJSON(unionJson).getBounds()];
+  return [unionJson, unionJson === null ? null : L.geoJSON(unionJson).getBounds()];
 };
 
 export default calcUnion;
